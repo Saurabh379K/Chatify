c11e34f67b86da76cec482ce97765abe5354477e
Saurabh379K <saurabhsemwal2003@gmail.com>
2025-12-23 15:44:08 +0530

    Sending welcome email

--- backend/src/controllers/auth.contoller.js
+++ backend/src/controllers/auth.contoller.js
@@ -1,6 +1,8 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { generateToken} from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { ENV } from "../lib/env.js"
 
 export const signup = async (req,res) => {
     const {fullName, email, password} = req.body
@@ -33,15 +35,22 @@ export const signup = async (req,res) => {
         })
 
         if(newUser) {
-            generateToken(newUser._id, res)
-            await newUser.save()
+            const savedUser = await newUser.save();
+            generateToken(newUser._id, res);
+            
 
             res.status(201).json({
                 _id: newUser._id,
                 fullName:newUser.fullName,
                 email:newUser.email,
                 profilePic:newUser.profilePic,
-            })
+            });
+
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.error("Failed to send welcome email:", error);
+            }
         }
         else {
             res.status(400).json({message: "Invalid user data"})
