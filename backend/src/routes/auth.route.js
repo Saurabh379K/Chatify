ffbee392ed550d284cf0a4d7236d78513d5bf6b2
Saurabh379K <saurabhsemwal2003@gmail.com>
2025-12-24 23:25:43 +0530

    update profile pic and added a middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.contoller.js";
+import { signup, login, logout, updateProfile } from "../controllers/auth.contoller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup)
-router.post("/login", login)
-router.post("/logout", logout)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req,res) => {
+    // @ts-ignore - req.user is added by protectRoute middleware
+    res.status(200).json(req.user);
+});
 
 export default router;
