0300adfcb791376259cad2615083b9932341cd75
Saurabh379K <saurabhsemwal2003@gmail.com>
2025-12-26 17:41:02 +0530

    message model, route and controller implemented

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { getAllContacts, getMesagesByUserId, sendMessage, getChatPartners } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send",  (req,res) => {
-    res.send("Send message endpoint");
-})
+// the middlewares execute in order
+router.use(arcjetProtection , protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMesagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
