d7ed3ff0c8b0fe312e75a92b7c3a30f61c536d3c
Saurabh379K <saurabhsemwal2003@gmail.com>
2026-01-04 15:04:13 +0530

    real time messages implemented

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -3,6 +3,8 @@ import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios';
 import { useAuthStore } from './useAuthStore';
 
+const notificationSound = new Audio("/sounds/notification.mp3")
+
 export const useChatStore = create((set,get) => ({
     allContacts: [],
     chats: [],
@@ -82,5 +84,30 @@ export const useChatStore = create((set,get) => ({
             set({ messages: messages })
             toast.error(error.response?.data?.message || "Failed to send message");
         }
-    }
+    },
+
+    subscribeToMessages: () => {
+        const { selectedUser, isSoundEnabled } = get();
+        if(!selectedUser) return;
+
+        const socket = useAuthStore.getState().socket;
+
+        socket.on("newMessage", (newMessage) => {
+            const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+            if (!isMessageSentFromSelectedUser) return;
+            
+            const currentMessages = get().messages
+            set({messages: [...currentMessages, newMessage]})
+
+            if(isSoundEnabled) {
+                notificationSound.currentTime = 0
+                notificationSound.play().catch((e) => console.log("Audio play failed", e));
+            }
+        })
+    },
+
+    unsubscribeFromMessages: () => {
+        const socket = useAuthStore.getState().socket;
+        socket.off("newMessage");
+    },
 }))
